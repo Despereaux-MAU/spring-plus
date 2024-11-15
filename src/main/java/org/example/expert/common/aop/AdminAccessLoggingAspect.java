b0e8588f2d9b96c4ccebe9c67bce69647fa58260
Despereaux <rlaehdwn025@naver.com>
2024-11-15 16:44:27 +0900

    fix : UserAdminController.class changeUserRole 메서드 AOP 개발 의도에 맞도록 수정

--- src/main/java/org/example/expert/common/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/common/aop/AdminAccessLoggingAspect.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,13 +19,23 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    public void logBeforeChangeUserAdminRole(JoinPoint joinPoint) {
+        String userRole = String.valueOf(request.getAttribute("userRole"));
+        String requestUrl = request.getRequestURI();
+        LocalDateTime requestTime = LocalDateTime.now();
+
+        log.info("Admin Access Before Log - User Role: {}, Request Time: {}, Request URL: {}, Method: {}",
+                userRole, requestTime, requestUrl, joinPoint.getSignature().getName());
+    }
+
     @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
-        log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
+        log.info("Admin Access After Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
                 userId, requestTime, requestUrl, joinPoint.getSignature().getName());
     }
 }
