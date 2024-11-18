8527165acae5b92e7437be15a3b74c2df26dd5d2
Despereaux <rlaehdwn025@naver.com>
2024-11-18 18:11:31 +0900

    refactor : QueryDsl 사용하기 위해 의존성 추가 후 리팩토링 chore : JPQL -> QueryDsl feat : N+1 문제 해결(fetchJoin)

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryDslRepository {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user WHERE " +
             "(:weather IS NULL OR t.weather = :weather) AND " +
@@ -22,9 +21,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             @Param("startDateTime") LocalDateTime startDateTime,
             @Param("endDateTime") LocalDateTime endDateTime,
             Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
